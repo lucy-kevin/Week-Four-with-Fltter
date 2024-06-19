b9c7f36e11d619dfc4ee9e999bee0f51e50e828f
lucy-kevin <kayikayikevin7@gmail.com>
2024-06-19 17:56:30 +0300

    week four

--- for_loop.dart
+++ for_loop.dart
@@ -1,7 +1,10 @@
 void main(){
-  var message = StringBuffer("Dart is fun");
+var callbacks = [];
+for (var i = 0; i < 2; i++) {
+  callbacks.add(() => print(i));
+}
 
-  for( var i =0; i < 5; i++){
-    message.write('!');
-  }  
+for (final c in callbacks) {
+  c();
+}
 }
